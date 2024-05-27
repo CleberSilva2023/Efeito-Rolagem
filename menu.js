5e18d7ee61d161ac4a2fec311c6abe9c0bc1b0bb
CleberSilva <clebercapricornio@gmail.com>
2024-05-27 17:07:38 -0300

    atualizei

--- menu.js
+++ menu.js
@@ -1,5 +1,5 @@
 
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 450)
+    header.classList.toggle('rolagem', window.scrollY > 0)
 });
